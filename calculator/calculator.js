87cc7c17b0ec34eecfec798299cb4d17aa43eaef
Paige Wilson <hello.pmwilson@gmail.com>
2023-11-13 20:52:38 -0500

    added one dynamic function to perform operations

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,19 +3,26 @@ var numToDisplay = ""; //set the starting display
 let previousNum = 0; //set first number
 var clearAfterClick = false; //if true, clear the display after button click
 var previousOperation = "";
+var operation = '';
+const operationDictionary = {
+    'add' : addNumbers,
+    'subtract' : subtractNumbers,
+    'multiply' : multiplyNumbers,
+    'divide' : divideNumbers,
+}
 
 
 function appendNum(event) { 
     //when clicked, check and clear if display needs to be cleared
     if (clearAfterClick == true) {
+        console.log("clearafterclick was true, new number started");
         clearDisplay();
         clearAfterClick = false;
     }
     //when clicked, append the number and display it
     numToDisplay += event.target.value; 
-        console.log("this happened");
+    console.log("number added");
     displayNumber(); 
-
 } 
 
 function displayNumber() { 
@@ -27,9 +34,30 @@ function clearDisplay() {
     numToDisplay = "";
     displayNumber();
 }
-function addNumber() {
-    console.log("add button hit");
-    var operation = "add";
+
+function addNumbers(a,b) {
+    result = parseFloat(a) + parseFloat(b);
+    return result;
+}
+
+function subtractNumbers(a,b) {
+    result = parseFloat(a) - parseFloat(b);
+    return result;
+}
+
+function multiplyNumbers(a,b) {
+    result = parseFloat(a) * parseFloat(b);
+    return result;
+}
+
+function divideNumbers(a,b) {
+    result = parseFloat(a) / parseFloat(b);
+    return result;
+}
+
+function doOp(event) {
+    operation = operationDictionary[event.target.value];
+    console.log(operation);
     //accidental button hit before number input
     if (numToDisplay == "") {
         //pass
@@ -37,19 +65,18 @@ function addNumber() {
     } else {
         console.log("numtodisplay not blank");
         //check if a different operator has been clicked previously
-        if (previousOperation == "add") {
-                console.log("previous operation is add, act as equal sign in running total");
+        if (previousOperation != "") {
+            console.log("previous operation is not blank, act as equal sign in running total");
             //if multiple operations are happening in a row
             //display the running total as new operators are hit
-            numToDisplay = parseFloat(previousNum) + parseFloat(numToDisplay);
-            displayNumber();
-            previousOperation = "add";
+            console.log(previousOperation);
+            numToDisplay = previousOperation(parseFloat(previousNum),parseFloat(numToDisplay));
+            displayNumber(); //I believe we need to change this so that you feed the display number in instead of setting it
             setUpOperation(operation);
         } else {
             console.log("first time add is hit, set up future operation");
             //if not doing a running total
             //set previousOperation to add for future operator button presses
-            previousOperation = "add";
             setUpOperation(operation);
         }
         
@@ -79,10 +106,13 @@ document.addEventListener('DOMContentLoaded', () => {
     //get number buttons and add event listeners
     const numberButtons = document.getElementsByClassName('numberButton');
     for (var i = 0; i < numberButtons.length; i++) {
-        numberButtons[i].addEventListener('click', appendNum)
-    };  
+        numberButtons[i].addEventListener('click', appendNum);
+    } 
     
-    //add button
-    const addButton = document.getElementById('add');
-    addButton.addEventListener('click', addNumber);
+    const operationButtons = document.getElementsByClassName('calculatorButton');
+    for (var i = 0; i < operationButtons.length; i++) {
+        operationButtons[i].addEventListener('click', doOp);
+    }
 })
+
+
