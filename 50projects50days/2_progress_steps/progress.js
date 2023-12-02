515d6996a7bdd5ee039b5767ff2b36fccf729e04
Paige Wilson <hello.pmwilson@gmail.com>
2023-12-02 12:07:32 -0500

    added function to change color on button press

--- 50projects50days/2_progress_steps/progress.js
+++ 50projects50days/2_progress_steps/progress.js
@@ -1,11 +1,37 @@
 let activeCircle = 1
 
-document.addEventListener('DOMContentLoaded', () => {
-    const circles = document.getElementByClass('circle');
+const circles = document.querySelectorAll('.circle');
 
-    const next = document.getElementById('next')
-    const prev = document.getElementById('prev')
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
 
-    
+next.addEventListener('click', () => {
+    activeCircle++;
 
+    if(activeCircle > circles.length) {
+        activeCircle = circles.length;
+    }
+
+    progress();
 })
+
+prev.addEventListener('click', () => {
+    activeCircle--
+
+    if (activeCircle < 1) {
+        activeCircle=1;
+    }
+    progress()
+}) 
+
+function progress() {
+    circles.forEach((circle, idx) => {
+        if(idx < activeCircle) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+}
+
+progress();
