7b1a7c379f2d4c6ee57c09a33b6b9d45b21f1a14
Paige Wilson <hello.pmwilson@gmail.com>
2023-12-01 19:26:11 -0500

    added js, added references, added next button event listener

---
+++ 50projects50days/2_progress_steps/progress.js
@@ -0,0 +1,11 @@
+let activeCircle = 1
+
+document.addEventListener('DOMContentLoaded', () => {
+    const circles = document.getElementByClass('circle');
+
+    const next = document.getElementById('next')
+    const prev = document.getElementById('prev')
+
+    
+
+})
