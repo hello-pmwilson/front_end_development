aa47aab828e69e34a829f496c6c1409361af7656
Paige Wilson <hello.pmwilson@gmail.com>
2023-12-05 19:40:55 -0500

    current working version

--- 50projects50days/3_rotating_navigation/rotate.js
+++ 50projects50days/3_rotating_navigation/rotate.js
@@ -3,14 +3,26 @@ const navigationClose = document.getElementById("close-nav");
 
 const navigation = document.querySelector(".nav");
 const article = document.querySelector(".article");
+const navList = document.querySelector(".nav-list");
+const navListList = navList.children;
 
 navigationOpen.addEventListener("click", () => {
     article.style.transform = 'rotate(-15deg)';
     navigation.style.transform = 'translate(-50%, -50%) rotate(-90deg)'
+    for (var i=0; i < navListList.length; i++) {
+        var offset = 150;
+        navListList[i].style.transform = 'translate(' + offset + '%)';
+        offset += 50;
+        navListList[i].style.transitionDelay = '0.5s';
+    }
 })
 
 navigationClose.addEventListener("click", () => {
     article.style.transform = '';
     navigation.style.transform = ''   
+    for (var i=0; i < navListList.length; i++) {
+        navListList[i].style.transform = '';
+        navListList[i].style.transitionDelay = '0s';
+    }
 })
 
