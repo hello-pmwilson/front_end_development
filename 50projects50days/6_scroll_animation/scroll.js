08ff634b207ce0da8d8db3861df0bccf35f3eaa6
Paige Wilson <hello.pmwilson@gmail.com>
2023-12-10 11:10:09 -0500

    added working remove content function, added scroll event listener, then broke it

--- 50projects50days/6_scroll_animation/scroll.js
+++ 50projects50days/6_scroll_animation/scroll.js
@@ -1,25 +1,53 @@
 const wrapper = document.getElementById('wrapper');
+const startWith = 3;
+var idx = 0;
 
-var count = 0;
+//on load, animate in the starting amount
+for (var i=0; i<startWith; i++) {
+    addContent();
+}
 
-function addContent() {
-    count ++;
+window.addEventListener('scroll', () => {
+    let scroll = window.scrollY;
+    desiredCount = Math.floor(scroll/200);
+    currentCount = count-startWith;
+    if (desiredCount > currentCount) {
+        dif = desiredCount - currentCount;
+        for (var i = 0; i < dif; i++) {
+            addContent();
+        }
+    }
+    if (desiredCount < currentCount) {
+        dif = currentCount - desiredCount;
+        for (var i = 0; i < dif; i++) {
+            removeContent();
+        }
+    }
+
+
+})
+
+function addContent(idx) {
+    idx++
+
     const content = document.createElement('div');
     const innerContent = document.createElement('h1');
-    innerContent.innerHTML = 'Content';
+    innerContent.innerHTML = 'Content'; 
+    content.appendChild(innerContent); 
 
-    let side = whatSide(count);
+    let side = whatSide(idx);
     content.classList = `content off-screen-${side}`;
-    content.appendChild(innerContent);
+    
     wrapper.appendChild(content);
     setTimeout(function () {
         content.classList.add(`show-${side}`);
     }, 0); //wait for the block of code to finish        
 
+
 }
 
-function whatSide(count) {
-    if (count % 2 === 0) {
+function whatSide(idx) {
+    if (idx % 2 === 0) {
         //even, come from the left
         return 'left';
     }
@@ -28,3 +56,14 @@ function whatSide(count) {
         return 'right';
     }
 }
+
+function removeContent(idx) {
+    const allContent = document.querySelectorAll('.content');
+    let side = whatSide();
+    idx--    
+    allContent[idx].classList.remove(`show-${side}`);
+    setTimeout(function () {
+        wrapper.removeChild(allContent[idx]);
+    }, 500); //wait for the block of code to finish
+}
+
