eafed660c47e9914f4c24d3f0c1fa003050aaa05
Paige Wilson <hello.pmwilson@gmail.com>
2023-12-12 20:20:20 -0500

    added style

--- 50projects50days/7_split_landing/split.js
+++ 50projects50days/7_split_landing/split.js
@@ -3,49 +3,47 @@
 //whereas when I'm moving toward the right it feels more responsive
 //I would switch to mouse over instead if there were more content
 
-//Alternative, mouse over available at the bottom
+//EDIT: I added more content and switched to mouseover
+
+//Alternative, mouse tracking available at the bottom
 
 const right = document.querySelector('.right');
 const left = document.querySelector('.left');
 
-//for implementing mouse over, start comment here
-document.addEventListener('mousemove', trackMouse);
-function trackMouse(event) {
-    const mouseX = event.clientX;
-    const screenWidth = window.innerWidth;
-    console.log(mouseX);
+left.addEventListener('mouseover', () => {
+    //mouse is on the left
+    left.style.width = '75%';
+    right.style.width = '25%';
+})
 
-    if (mouseX <= screenWidth / 2) {
-        //mouse is on the left
-        left.style.width = '75%';
-        right.style.width = '25%';
-    } else if (mouseX >= screenWidth / 2) {
-        //mouse is on the right
-        left.style.width = '25%';        
-        right.style.width = '75%';
-    } else {
-        left.style.width = '50%';        
-        right.style.width = '50%';
-    }
-    
-}
-//end comment here
+right.addEventListener('mouseover', () => {
+    left.style.width = '25%';        
+    right.style.width = '75%';
+})
 
 document.addEventListener('mouseout', () => {
     left.style.width = '50%';        
     right.style.width = '50%';
 })
 
+//ALTERNATIVE TRACK MOUSE
+// document.addEventListener('mousemove', trackMouse);
+// function trackMouse(event) {
+//     const mouseX = event.clientX;
+//     const screenWidth = window.innerWidth;
+//     console.log(mouseX);
 
-//ALTERNATIVE USING MOUSE OVER DIV
-
-// left.addEventListener('mouseover', () => {
-//     //mouse is on the left
-//     left.style.width = '75%';
-//     right.style.width = '25%';
-// })
-
-// right.addEventListener('mouseover', () => {
-//     left.style.width = '25%';        
-//     right.style.width = '75%';
-// })
+//     if (mouseX <= screenWidth / 2) {
+//         //mouse is on the left
+//         left.style.width = '75%';
+//         right.style.width = '25%';
+//     } else if (mouseX >= screenWidth / 2) {
+//         //mouse is on the right
+//         left.style.width = '25%';        
+//         right.style.width = '75%';
+//     } else {
+//         left.style.width = '50%';        
+//         right.style.width = '50%';
+//     }
+    
+// }
