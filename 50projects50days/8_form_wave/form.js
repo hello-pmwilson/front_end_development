32277609eda5e9a385a09cfd278c07e9a087eb2c
Paige Wilson <hello.pmwilson@gmail.com>
2023-12-16 20:45:59 -0500

    changed the placeholder to spans. fixed white space between spans.

--- 50projects50days/8_form_wave/form.js
+++ 50projects50days/8_form_wave/form.js
@@ -1,1 +1,9 @@
-console.log("Hello, world.");
+const email = document.getElementById('email');
+const pass = document.getElementById('password');
+
+const emailSpan = document.getElementById('email-span');
+const passSpan = document.getElementById('password-span');
+
+email.addEventListener('focus', ()=>{
+    
+})
